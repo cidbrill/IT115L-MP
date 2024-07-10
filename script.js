c8226d684494c69ae33b6f0e9956b15c3a81e9d9
cidbrill <iam.cidbrill@gmail.com>
2024-07-10 12:32:35 +0800

    Project Update Restructured API function calls to properly validate input data within the forms

--- script.js
+++ script.js
@@ -49,6 +49,31 @@ function switchToAddRecord() {
     document.getElementById('insert-record').style.display = 'flex';
 }
 
+window.onload = function() {
+    if (localStorage.getItem('currentUser')) {
+        switchToPanelsPage(localStorage.getItem('currentUser'));
+    }
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('signin-page').style.display = 'flex';
+
+    const signupPage = document.getElementById('signup-page');
+    const signinPage = document.getElementById('signin-page');
+    const switchToSignIn = document.getElementById('switchToSignIn');
+    const switchToSignUp = document.getElementById('switchToSignUp');
+
+    switchToSignIn.addEventListener('click', function() {
+        signupPage.style.display = 'none';
+        signinPage.style.display = 'flex';
+    });
+
+    switchToSignUp.addEventListener('click', function() {
+        signinPage.style.display = 'none';
+        signupPage.style.display = 'flex';
+    });
+});
+
 document.addEventListener('DOMContentLoaded', async function() {
     try {
         const response = await fetch('http://localhost:3000/select/eventname');
@@ -71,219 +96,207 @@ document.addEventListener('DOMContentLoaded', async function() {
     }
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById('signin-page').style.display = 'flex';
+async function signUp(event) {
+    event.preventDefault();
 
-    const signupPage = document.getElementById('signup-page');
-    const signinPage = document.getElementById('signin-page');
-    const switchToSignIn = document.getElementById('switchToSignIn');
-    const switchToSignUp = document.getElementById('switchToSignUp');
+    const username = document.getElementById('signup-studnum').value;
+    const password = document.getElementById('signup-password').value;
 
-    switchToSignIn.addEventListener('click', function() {
-        signupPage.style.display = 'none';
-        signinPage.style.display = 'flex';
-    });
+    if (username.substring(0, 5).toLowerCase() === 'admin') {
+        alert('You may not register with the following username.');
+        return;
+    }
 
-    switchToSignUp.addEventListener('click', function() {
-        signinPage.style.display = 'none';
-        signupPage.style.display = 'flex';
-    });
+    try {
+        const response = await fetch('http://localhost:3000/auth/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        });
 
-    document.getElementById('signup-button').addEventListener('click', async function(event) {
-        event.preventDefault();
-        const username = document.getElementById('signup-studnum').value;
-        const password = document.getElementById('signup-password').value;
+        const result = await response.text();
+        alert(result);
 
-        if (username.substring(0, 5).toLowerCase() === 'admin') {
-            alert('You may not register with the following username.');
-            return;
+        if (response.ok) {
+            localStorage.setItem('currentUser', username);
+            switchToCredentialsPage();
         }
+    } catch (error) {
+        alert('An error occurred. Please try again later.');
+    }
+}
 
-        try {
-            const response = await fetch('http://localhost:3000/auth/signup', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ username, password })
-            });
+async function signIn(event) {
+    event.preventDefault();
 
-            const result = await response.text();
-            alert(result);
+    const username = document.getElementById('signin-studnum').value;
+    const password = document.getElementById('signin-password').value;
 
-            if (response.ok) {
-                switchToCredentialsPage();
-            }
-        } catch (error) {
-            alert('An error occurred. Please try again later.');
+    try {
+        const response = await fetch('http://localhost:3000/auth/signin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        });
+
+        const result = await response.text();
+        alert(result);
+
+        if (response.ok) {
+            localStorage.setItem('currentUser', username);
+            switchToPanelsPage(username);
         }
-    });
+    } catch (error) {
+        alert('An error occurred. Please try again later.');
+    }
+}
 
-    document.getElementById('signin-button').addEventListener('click', async function(event) {
-        event.preventDefault();
-        const username = document.getElementById('signin-studnum').value;
-        const password = document.getElementById('signin-password').value;
+function signOut() {
+    localStorage.clear();
+    location.reload();
+}
 
-        try {
-            const response = await fetch('http://localhost:3000/auth/signin', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ username, password })
-            });
+async function submitCredentials(event) {
+    event.preventDefault();
 
-            const result = await response.text();
-            alert(result);
+    const username = localStorage.getItem('currentUser');
+    const givenName = document.getElementById('given-name').value;
+    const lastName = document.getElementById('last-name').value;
+    const program = document.querySelector('input[name="program"]:checked').value;
+    const house = document.querySelector('select[name="house"]').value;
 
-            if (response.ok) {
-                switchToPanelsPage(username);
-            }
-        } catch (error) {
-            alert('An error occurred. Please try again later.');
+    try {
+        const response = await fetch('http://localhost:3000/insert/credentials', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, givenName, lastName, program, house })
+        });
+
+        const result = await response.text();
+        alert(result);
+
+        if (response.ok) {
+            switchToPanelsPage(username);
         }
-    });
+    } catch (error) {
+        alert('An error occurred. Please try again later.');
+    }
+}
 
-    document.getElementById('subcred-button').addEventListener('click', async function(event) {
-        event.preventDefault();
-        const username = document.getElementById('signup-studnum').value;
-        const givenName = document.getElementById('given-name').value;
-        const lastName = document.getElementById('last-name').value;
-        const program = document.querySelector('input[name="program"]:checked').value;
-        const house = document.querySelector('select[name="house"]').value;
+async function searchRecord(event) {
+    event.preventDefault();
 
-        try {
-            const response = await fetch('http://localhost:3000/insert/credentials', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ username, givenName, lastName, program, house })
-            });
+    const username = document.getElementById('search-studnum').value;
+    const eventCode = document.getElementById('searchEventName').value;
+    const dateAttended = document.getElementById('searchDateAttended').value;
+    const participation = document.querySelector('input[name="searchParticipation"]:checked').value;
 
-            const result = await response.text();
-            alert(result);
+    try {
+        const response = await fetch('http://localhost:3000/select/record', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, eventCode, dateAttended, participation })
+        });
 
-            if (response.ok) {
-                switchToPanelsPage(username);
-            }
-        } catch (error) {
-            alert('An error occurred. Please try again later.');
+        const result = await response.text();
+        alert(result);
+
+        if (response.ok) {
+            returnToPanelsPage(document.getElementById('signin-studnum').value);
         }
-    });
+    } catch (error) {
+        alert('An error occurred. Please try again later.');
+    }
+}
 
-    document.getElementById('searchrec-button').addEventListener('click', async function(event) {
-        event.preventDefault();
-        const username = document.getElementById('search-studnum').value;
-        const eventCode = document.getElementById('searchEventName').value;
-        const dateAttended = document.getElementById('searchDateAttended').value;
-        const participation = document.querySelector('input[name="searchParticipation"]:checked').value;
+async function searchRecordToUpdate(event) {
+    event.preventDefault();
 
-        try {
-            const response = await fetch('http://localhost:3000/select/record', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ username, eventCode, dateAttended, participation })
-            });
+    const username = document.getElementById('update-studnum').value;
+    const eventCode = document.getElementById('updateEventName').value;
+    const dateAttended = document.getElementById('iUpdateDateAttended').value;
+    const participation = document.querySelector('input[name="iUpdateParticipation"]:checked').value;
 
-            const result = await response.text();
-            alert(result);
+    try {
+        const response = await fetch('http://localhost:3000/select/record', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, eventCode, dateAttended, participation })
+        });
 
-            if (response.ok) {
-                returnToPanelsPage(document.getElementById('signin-studnum').value);
-            }
-        } catch (error) {
-            alert('An error occurred. Please try again later.');
+        const result = await response.text();
+        alert(result);
+
+        if (response.ok) {
+            switchToFUpdateRecord();
         }
-    });
+    } catch (error) {
+        alert('An error occurred. Please try again later.');
+    }
+}
 
-    document.getElementById('searchRecToUp-button').addEventListener('click', async function(event) {
-        event.preventDefault();
-        const username = document.getElementById('update-studnum').value;
-        const eventCode = document.getElementById('updateEventName').value;
-        const dateAttended = document.getElementById('iUpdateDateAttended').value;
-        const participation = document.querySelector('input[name="iUpdateParticipation"]:checked').value;
+async function updateRecord(event) {
+    event.preventDefault();
 
-        try {
-            const response = await fetch('http://localhost:3000/select/record', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ username, eventCode, dateAttended, participation })
-            });
+    const username = document.getElementById('update-studnum').value;
+    const eventCode = document.getElementById('updateEventName').value;
+    const dateAttended = document.getElementById('fUpdateDateAttended').value;
+    const participation = document.querySelector('input[name="fUpdateParticipation"]:checked').value;
 
-            const result = await response.text();
-            alert(result);
+    try {
+        const response = await fetch('http://localhost:3000/update/record', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, eventCode, dateAttended, participation })
+        });
 
-            if (response.ok) {
-                switchToFUpdateRecord();
-            }
-        } catch (error) {
-            alert('An error occurred. Please try again later.');
+        const result = await response.text();
+        alert(result);
+
+        if (response.ok) {
+            returnToPanelsPage(document.getElementById('signin-studnum').value)
         }
-    });
+    } catch (error) {
+        alert('An error occurred. Please try again later.');
+    }
+}
 
-    document.getElementById('updaterec-button').addEventListener('click', async function(event) {
-        event.preventDefault();
-        const username = document.getElementById('update-studnum').value;
-        const eventCode = document.getElementById('updateEventName').value;
-        const dateAttended = document.getElementById('fUpdateDateAttended').value;
-        const participation = document.querySelector('input[name="fUpdateParticipation"]:checked').value;
+async function insertRecord(event) {
+    event.preventDefault();
 
-        try {
-            const response = await fetch('http://localhost:3000/update/record', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ username, eventCode, dateAttended, participation })
-            });
+    const username = localStorage.getItem('currentUser');
+    const eventCode = document.getElementById('insertEventName').value;
+    const dateAttended = document.getElementById('insertDateAttended').value;
+    const participation = document.querySelector('input[name="insertParticipation"]:checked').value;
 
-            const result = await response.text();
-            alert(result);
+    try {
+        const response = await fetch('http://localhost:3000/insert/record', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, eventCode, dateAttended, participation })
+        });
 
-            if (response.ok) {
-                returnToPanelsPage(document.getElementById('signin-studnum').value)
-            }
-        } catch (error) {
-            alert('An error occurred. Please try again later.');
+        const result = await response.text();
+        alert(result);
+
+        if (response.ok) {
+            returnToPanelsPage(document.getElementById('signin-studnum').value)
         }
-    });
-
-    document.getElementById('insrec-button').addEventListener('click', async function(event) {
-        event.preventDefault();
-        let username;
-
-        if (document.getElementById('signup-studnum').value === '') {
-            username = document.getElementById('signin-studnum').value
-        } else if (document.getElementById('signin-studnum').value === '') {
-            username = document.getElementById('signup-studnum').value
-        }
-
-        const eventCode = document.getElementById('insertEventName').value;
-        const dateAttended = document.getElementById('insertDateAttended').value;
-        const participation = document.querySelector('input[name="insertParticipation"]:checked').value;
-
-        try {
-            const response = await fetch('http://localhost:3000/insert/record', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ username, eventCode, dateAttended, participation })
-            });
-
-            const result = await response.text();
-            alert(result);
-
-            if (response.ok) {
-                returnToPanelsPage(document.getElementById('signin-studnum').value)
-            }
-        } catch (error) {
-            alert('An error occurred. Please try again later.');
-        }
-    });
-});
+    } catch (error) {
+        alert('An error occurred. Please try again later.');
+    }
+}
